03ac463f98bd582219570fe1347d6df0a610007e
MamakovAV <MamakovAV@W12MAMAKOVAV>
2017-03-31 10:24:21 +0300

    button2

--- sfml-test02/sfGUI.h
+++ sfml-test02/sfGUI.h
@@ -10,15 +10,24 @@ public:
 	sfGUI(const sfGUI&) = delete;
 	sfGUI& operator=(const sfGUI&) = delete;
 	sfGUI() {
+
 		label = sfg::Label::Create("Hello world!");
-		sfg::Label::Ptr _label = label;
+
+		//std::string str = ;
+		label2 = sfg::Label::Create("Click count = " + std::to_string(click_count));
+		//sfg::Label::Ptr _label = label;
 		
 		button = sfg::Button::Create("Greet SFGUI!");
-		button->GetSignal(sfg::Widget::OnLeftClick).Connect([_label] { _label->SetText("Hello SFGUI, pleased to meet you!"); });
+		button->GetSignal(sfg::Widget::OnLeftClick).Connect(std::bind(&sfGUI::button_OnClick, this));
+		button2 = sfg::Button::Create("ClickMe!");
+		button2->GetSignal(sfg::Widget::OnLeftClick).Connect(std::bind(&sfGUI::button2_OnClick, this));
+			//Connect([_label] { _label->SetText("Hello SFGUI, pleased to meet you!"); });
 
 		box = sfg::Box::Create(sfg::Box::Orientation::VERTICAL, 10.0f);
 		box->Pack(label);
 		box->Pack(button, false);
+		box->Pack(label2);
+		box->Pack(button2, false);
 
 		window = sfg::Window::Create();
 		window->SetTitle("Hello world!"); 
@@ -39,12 +48,24 @@ public:
 		_sfgui.Display(target_window);
 	}
 private:
+	int click_count = 0;
 	sfg::SFGUI _sfgui;
 
 	sfg::Button::Ptr button;
+	sfg::Button::Ptr button2;
 	sfg::Label::Ptr label;
+	sfg::Label::Ptr label2;
 	sfg::Box::Ptr box;
 
 	sfg::Window::Ptr window;
 	sfg::Desktop desktop;
+
+	void button_OnClick() {
+		label->SetText("Hello SFGUI, pleased to meet you!");
+	}
+	void button2_OnClick() {
+		++click_count;
+		//std::string str = ;
+		label2->SetText("Click count = " + std::to_string(click_count));
+	}
 };
